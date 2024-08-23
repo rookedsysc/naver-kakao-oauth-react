486d79795d573a8ea6bc45b7bc1a8aceddd92de2
rookedsysc <rookedsysc36@gmail.com>
2024-08-23 14:44:13 +0900

    feat: Kakao OAuth 추가

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './components/Login';
 import Callback from './components/Callback';
+import KakaoCallback from "./components/KakaoCallback";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/callback" element={<Callback />} />
+        <Route path="/kakao/callback" element={<KakaoCallback />} /> {/* Kakao 콜백 경로 추가 */}
       </Routes>
     </Router>
   );
